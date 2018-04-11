b4b2b910c17de0d51be3938ab3db4c68b2f6bdd2
Maxim Filimonov <tpaktopsp@gmail.com>
2018-04-11 20:04:57 +0800

    add more plugins

--- .hyper.js
+++ .hyper.js
@@ -104,7 +104,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-search', 'hypercwd', 'hyperlinks'],
+  plugins: ['hyper-search', 'hypercwd', 'hyperlinks', 'hypertheme', 'hyper-one-light'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
